5a0908a9b5cd9697d5931a80ae762ccb248ea872
aman kushwah <ak4677@srmist.edu.in>
2025-08-30 01:07:13 +0530

    fix error

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 const API_BASE = import.meta.env.VITE_API_BASE_URL;
 export async function fetchAttendance() {
-  const res = await fetch(`${API_BASE}/attendance`);
+  const res = await fetch(`${API_BASE}`);
   if (!res.ok) throw new Error("Failed to fetch");
   return res.json();
 }
 
 export async function saveAttendance(subjects) {
-  const res = await fetch(`${API_BASE}/attendance`, {
+  const res = await fetch(`${API_BASE}`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ subjects }),
